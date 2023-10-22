42187d1f2daa0f6e63a91e34ec4e9576c13d726c
Edglei Marques <edgleitab@gmail.com>
2023-10-22 15:41:37 -0300

    Subindo FrontEnd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes , Route } from "react-router-dom";
+import './Style/App.css';
+//import { useState } from 'react';
+import CadProduct from './Components/CadProduct';
+import Navbar from "./Layout/Navbar";
+import Login from "./Components/Login";
+import Product from "./Components/Product";
+import EditProduct from "./Components/editProduct";
+
 
 function App() {
+//  const[edg, setEdg] = useState("Edglei");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Login/>}/>
+            
+        <Route path="/CadProduct" element={<CadProduct/>}/>
+
+        <Route path="/Product" element={<Product/>}/>
+        <Route path="/EditProduct" element={<EditProduct/>}/>
+        
+            
+      </Routes>
+    </Router>    
   );
 }
 
